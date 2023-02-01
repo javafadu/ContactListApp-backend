e990cca923b2a924c8cd60f8d269f21c08771b62
javafadu <javafadu@gmail.com>
2023-02-01 10:04:11 +0300

    1Feb-1000

---
+++ src/test/java/com/contactlistapp/ContactListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contactlistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
