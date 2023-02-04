4c35f3cbf97a04be9a19dd125ac88f3c71c5c99d
javafadu <javafadu@gmail.com>
2023-02-04 13:34:59 +0300

    4Feb-1340

--- src/main/java/com/contactlistapp/controller/UserAuthController.java
+++ src/main/java/com/contactlistapp/controller/UserAuthController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/users")
 @AllArgsConstructor
--- src/main/java/com/contactlistapp/controller/UserJwtController.java
+++ src/main/java/com/contactlistapp/controller/UserJwtController.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping
 @AllArgsConstructor
